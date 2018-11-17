37aca89c024d09f328e48003c4158c36eade7308
tagirmi <tagirmi@gmail.com>
2018-11-17 22:34:07 +0300

    обработка команд

--- command_handler.h
+++ command_handler.h
@@ -2,6 +2,10 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
+
+#include "database.h"
+#include "util.h"
 
 namespace hw13 {
 
@@ -27,6 +31,31 @@ public:
         return {};
     }
 
+protected:
+    std::string ok()
+    {
+        return "OK\n";
+    }
+
+    std::string ok(const Database::Result& result)
+    {
+        std::stringstream ss;
+        for (const auto& i : result)
+            ss << i << "\n";
+        ss << "OK\n";
+        return ss.str();
+    }
+
+    std::string error(const std::string& message)
+    {
+        return "ERR " + message + "\n";
+    }
+
+    std::vector<std::string> parseArgs(const std::string& command)
+    {
+        return util::split(command, ' ');
+    }
+
 private:
     CommandHandler* m_next{};
 };
@@ -39,9 +68,42 @@ public:
         if (command.find("INSERT") != 0)
             return CommandHandler::exec(command);
 
-        //TODO handle INSERT
-        std::cout << "handle insert\n";
-        return "OK\n";
+        auto args = parseArgs(command);
+
+        if (args.size() != 4)
+            return error("Неверное количество аргументов");
+
+        int id{};
+        std::string parseError;
+        if (!parseId(args[2], id, parseError))
+            return error(parseError);
+
+        auto& db = Database::instance();
+
+        if (!db.insert(args[1], std::make_tuple(id, args[3])))
+            return error(db.getError());
+
+        return ok();
+    }
+
+private:
+    bool parseId(const std::string& id, int& parsed, std::string& error)
+    {
+        try
+        {
+            parsed = std::stoi(id);
+        }
+        catch(const std::invalid_argument&)
+        {
+            error = "Аргумент ID неверного типа";
+            return false;
+        }
+        catch(const std::out_of_range&)
+        {
+            error = "Невозможно преобразовать аргумент ID в число";
+            return false;
+        }
+        return true;
     }
 };
 
@@ -54,9 +116,17 @@ public:
         if (command.find("TRUNCATE") != 0)
             return CommandHandler::exec(command);
 
-        //TODO handle TRUNCATE
-        std::cout << "handle truncate\n";
-        return "OK\n";
+        auto args = parseArgs(command);
+
+        if (args.size() != 2)
+            return error("Неверное количество аргументов");
+
+        auto& db = Database::instance();
+
+        if (!db.truncate(args[1]))
+            return error(db.getError());
+
+        return ok();
     }
 };
 
@@ -68,9 +138,17 @@ public:
         if (command.find("INTERSECTION") != 0)
             return CommandHandler::exec(command);
 
-        //TODO handle INTERSECTION
-        std::cout << "handle intersection\n";
-        return "OK\n";
+        auto args = parseArgs(command);
+
+        if (args.size() > 1)
+            return error("Аргументы не ожидаются");
+
+        auto& db = Database::instance();
+
+        if (!db.intersection())
+            return error(db.getError());
+
+        return ok(db.getResult());
     }
 };
 
@@ -82,9 +160,17 @@ public:
         if (command.find("SYMMETRIC_DIFFERENCE") != 0)
             return CommandHandler::exec(command);
 
-        //TODO handle SYMMETRIC_DIFFERENCE
-        std::cout << "handle symmetric difference\n";
-        return "OK\n";
+        auto args = parseArgs(command);
+
+        if (args.size() > 1)
+            return error("Аргументы не ожидаются");
+
+        auto& db = Database::instance();
+
+        if (!db.symmetric_difference())
+            return error(db.getError());
+
+        return ok(db.getResult());
     }
 };
 
@@ -93,7 +179,7 @@ class UnknownCommandHandler : public CommandHandler
 public:
     std::string exec(const std::string&) override
     {
-        return "ERR unknown command\n";
+        return error("Неопознанная команда");
     }
 };
 
