538899453cdfea19fffa1b32de4d012a6dbb116d
tagirmi <tagirmi@gmail.com>
2018-11-17 22:32:43 +0300

    database

--- database.cpp
+++ database.cpp
@@ -1,1 +1,31 @@
 #include "database.h"
+
+bool hw13::Database::insert(const std::string &table, const std::tuple<int, std::string> &row)
+{
+    return false;
+}
+
+bool hw13::Database::truncate(const std::string &table)
+{
+    return false;
+}
+
+bool hw13::Database::intersection()
+{
+    return false;
+}
+
+bool hw13::Database::symmetric_difference()
+{
+    return false;
+}
+
+std::string hw13::Database::getError()
+{
+    return m_error;
+}
+
+const hw13::Database::Result &hw13::Database::getResult()
+{
+    return m_result;
+}
--- database.h
+++ database.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <tuple>
 
 namespace hw13 {
 
@@ -9,11 +10,6 @@ class Database
 {
 public:
     using Result = std::vector<std::string>;
-
-    enum class Table {
-        A,
-        B
-    };
 
     Database() = default;
     Database(const Database&) = delete;
@@ -25,13 +21,17 @@ public:
         return db;
     }
 
-    bool insert(Table, int id, const std::string& name);
-    bool truncate(Table);
+    bool insert(const std::string& table, const std::tuple<int, std::string>& row);
+    bool truncate(const std::string& table);
     bool intersection();
     bool symmetric_difference();
 
     std::string getError();
     const Result& getResult();
+
+private:
+    std::string m_error{};
+    Result m_result{};
 };
 
 } // hw13
